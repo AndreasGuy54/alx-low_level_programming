78f12f5f0db69702ee2ce0320ee57b3fc82af730
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-06-17 14:16:58 +0000

    Print sizes of data types fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,18 +7,10 @@
 
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
-	long longInt;
-	long long longlongInt;
-
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongInt));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
-	return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+        printf("Size of an int: %zu byte(s)\n", sizeof(int));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %zu byte(s)\n", sizeof(float));
+        return (0);
 }
