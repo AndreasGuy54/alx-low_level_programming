f93e15d3f59ef4a18b7ec98da46470aab6324eec
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-06-17 14:03:34 +0000

    Print sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+* main - entry point of program
+* Return: 0 to exit
+*/
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType));            
+	return 0;
+}
