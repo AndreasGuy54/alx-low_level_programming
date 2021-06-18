cb887fc84b343e389829073e57593bb3f4a4b294
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-06-18 12:17:40 +0000

    Last digit correct

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if ((n % 10) == 10)
 	{
-		printf("and is 0\n");
+		printf("0\n");
 	}
 	else if ((n % 10) > 5)
 	{
-		printf("and greater than 5\n");
+		printf("greater than 5\n");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("less than 6 and not 0\n");
 	}
 
 	return (0);
