24ba228ba229529642db1bb71f5940b30b2be265
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-06-18 13:23:33 +0000

    Print 00-99 and 0-9

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - print 00-99 and single digits, no duplicates, comma separated
+* Return: 0 to exit
+*/
+
+int main(void)
+{
+	int tens = '0';
+	int ones = tens + 1;
+
+	while (tens <= '9') /*increases tens until 9*/
+	{
+		while(ones <= '9') /*increases ones until 9*/
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			ones++;
+		}
+		tens++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
