495ca9f628329d3097eedafeb308c0afdc27e53a
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-06-18 13:09:32 +0000

    Print 0-9 separate by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - print 0-9, separate by commas
+* Return: 0 to exit
+*/
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9') /*prints 0-9*/
+	{
+		putchar(num);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		)
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
