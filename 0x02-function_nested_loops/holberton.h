f965d56e8b56f379ce1521a4daaea21e75ecea04
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-06-22 17:18:05 +0000

    Initial Header file, Function to print

---
+++ 0x02-function_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: 0 to exit.
+*/
+
+int _putchar(char c);
