bca488bd9cba76ea6e18bd6dd11b44039b2bb91f
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-06-22 18:35:50 +0000

    check for lowercase

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -19,4 +19,32 @@ void print_alphabet(void);
 */
 void print_alphabet_x10(void);
 
+/**
+* _islower - checks for lowercase character
+* @c: the character to check
+* Return: int
+*/
+int _islower(int c);
+
+/**
+* _isalpha - check for alphabet
+* @c: character to check
+* Return: 1 if lower or uppercase, 0 if not alphabet character
+*/
+int _isalpha(int c);
+
+/**
+* print_sign - print sign of number
+* @n: number to check
+* Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+*/
+int print_sign(int n);
+
+/**
+* _abs - absolute value of integer
+* @int: integer
+* Return: absolute value
+*/
+int _abs(int);
+
 #endif
